b2957e925769881952a6a73b12bf018693f1dbd7
sorc <sorc@qq.com>
2026-06-05 17:56:08 +0800

    fix(sandbox/docker): 修复容器内宿主机目录权限问题
    新增设置/home/sorc目录权限的逻辑，同时添加初始化脚本执行的日志提示

--- sandbox/docker/entrypoint.sh
+++ sandbox/docker/entrypoint.sh
@@ -6,9 +6,10 @@ if [ -S /var/run/docker.sock ]; then
   chown sorc:sorc /var/run/docker.sock
   chmod 660 /var/run/docker.sock
 fi
-
+chown sorc:sorc /home/sorc
 MARKER_FILE="/home/sorc/.container_initialized"
 if [ ! -f "$MARKER_FILE" ]; then
+  echo "=> Setting up init scripts"
   INIT_DIR="/docker-entrypoint-init.d"
   if [ -d "$INIT_DIR" ]; then
     for f in $(find "$INIT_DIR" -name '*.sh' | sort); do
